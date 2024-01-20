654eaf104dcaf8f9fc3790859a5777dcbc4267c5
zayedlovescats <154771540+zayedlovescats@users.noreply.github.com>
2024-01-19 23:04:38 -0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const darkModeToggle = document.getElementById('darkModeToggle');
-const body = document.body;
-
-darkModeToggle.addEventListener('change', () => {
-    body.classList.toggle('dark-mode');
-});
